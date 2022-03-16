ddaa69aeafd11d3d56ffbe5a72743a6a9f90f224
Monikelly <101274436+Monikelly@users.noreply.github.com>
2022-03-16 13:18:20 -0300

    Add files via upload

---
+++ hortmart/int.js
@@ -0,0 +1,59 @@
+
+
+var i = 1;
+var id = 'produto';
+var milissegundos = 2000; 
+
+
+var interval = setInterval(function(){
+
+  
+  var img = document.getElementById(id);
+
+  i++;
+  
+  
+  img.setAttribute('src', 'psd.jpg');
+
+  
+}, milissegundos);
+
+
+
+var i = 1;
+var id = 'produto';
+var milissegundos = 4000; 
+
+
+var interval = setInterval(function(){
+
+  
+  var img = document.getElementById(id);
+
+  i++;
+  
+  
+  img.setAttribute('src', 'psd2.jpg');
+
+  
+}, milissegundos);
+
+
+var i = 1;
+var id = 'produto';
+var milissegundos = 6000; 
+
+
+var interval = setInterval(function(){
+
+  
+  var img = document.getElementById(id);
+
+  i++;
+  
+  
+  img.setAttribute('src', 'packs.jpeg');
+
+  
+}, milissegundos);
+
